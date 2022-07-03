65f043c03864eb7a9a800fd19d5067a0e1bed1d1
Parinpond <parinpond.lag@gmail.com>
2022-07-03 11:55:24 +0700

    fix : indent

--- modules/Profile/addPhone.tsx
+++ modules/Profile/addPhone.tsx
@@ -1,73 +1,72 @@
 import React, { FC } from 'react'
 import Helmet from 'react-helmet'
 import { Typography, Button, Row, Col, Form, Input} from 'antd'
+import Link from 'next/link'
+import { useRouter, NextRouter } from 'next/router'
 import t from '~/locales'
 import styles from './Profile.module.scss'
 import SideBarSettingMenu from '~/components/main/SideBarSettingMenu'
-import Link from 'next/link'
-import { useRouter, NextRouter } from 'next/router'
+
 import { Url } from '~/utils/main'
 
 const { Text } = Typography
 
 const AddPhone: FC = () => {
-    const router: NextRouter = useRouter()
+  const router: NextRouter = useRouter()
   return (  
     <main className="main account">
-        <Helmet>
-            <title>{t('meta.title')} | {t('accountProfile.form.title')}</title>
-        </Helmet>
-        <nav className="breadcrumb-nav">
-            <div className="container">
-                <ul className="breadcrumb">
-                    <li><i className="d-icon-home"/></li>
-                    <li disabled>{t('accountProfile.form.setting')}</li>
-                    <li disabled>{t('accountProfile.form.title')}</li>
-                    <li><Link href={Url.href('/personal-info', router.locale)}>{t('accountProfile.form.personalInfo')}</Link></li>
-                    <li><Link href={Url.href('/personal-info/phone', router.locale)}>{t('accountProfile.phone.titleEdit')}</Link></li>
-                    <li><Link href={Url.href('/personal-info/add-phone', router.locale)}>{t('accountProfile.phone.titleAdd')}</Link></li>
-
-                </ul>
-            </div>
-        </nav>
-        <div className="page-content mb-9">
+      <Helmet><title>{t('meta.title')} | {t('accountProfile.form.title')}</title></Helmet>
+      <nav className="breadcrumb-nav">
         <div className="container">
-        <Row gutter={48}>
+          <ul className="breadcrumb">
+            <li><i className="d-icon-home"/></li>
+            <li disabled>{t('accountProfile.form.setting')}</li>
+            <li disabled>{t('accountProfile.form.title')}</li>
+            <li><Link href={Url.href('/personal-info', router.locale)}>{t('accountProfile.form.personalInfo')}</Link></li>
+            <li><Link href={Url.href('/personal-info/phone', router.locale)}>{t('accountProfile.phone.titleEdit')}</Link></li>
+            <li><Link href={Url.href('/personal-info/add-phone', router.locale)}>{t('accountProfile.phone.titleAdd')}</Link></li>
+
+          </ul>
+        </div>
+      </nav>
+      <div className="page-content mb-9">
+        <div className="container">
+          <Row gutter={48}>
             <Col xl={6} lg={0}>
-                <SideBarSettingMenu/>
+              <SideBarSettingMenu/>
             </Col> 
             <Col xl={{ span: 15, offset: 1 }} lg={{ span: 18, offset: 3 }} md={24}>
-                <Text>
+              <Text>
                 <h4 className={`text-center mb-5 ${styles.textPrimary}`}>{t('accountProfile.phone.titleAdd')}</h4>
-                </Text>
-                <Form layout="vertical">
-                    <Row gutter={[16, 8]}>
-                        <Col md={12} xs={24} offset="6">
-                            <Form.Item label={t('accountProfile.phone.newPhone')} name="phone">
-                            <Input maxLength={10} />
-                            </Form.Item>
-                        </Col>
-                        <Col md={12} xs={24} offset="6">
-                            <Form.Item>
-                            <Button htmlType="submit" className={styles.btnInfo} block>{t('accountProfile.button.sendVerificationCode')}</Button>
-                            </Form.Item>
-                        </Col>
-                        <Col md={12} xs={64} offset="6">
-                            <Form.Item label={t('accountProfile.phone.otp')} name="otp">
-                            <Input maxLength={10} />
-                            </Form.Item>
-                        </Col>
-                        <Col md={12} xs={24} offset="6">
-                            <Form.Item>
-                            <Button  className={styles.btnPrimary} block>{t('accountProfile.button.addPhone')}</Button>
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                </Form>
+              </Text>
+              <Form layout="vertical">
+                <Row gutter={[16, 8]}>
+                  <Col md={12} xs={24} offset="6">
+                    <Form.Item label={t('accountProfile.phone.newPhone')} name="phone">
+                      <Input maxLength={10} />
+                    </Form.Item>
+                  </Col>
+                  <Col md={12} xs={24} offset="6">
+                    <Form.Item>
+                      <Button htmlType="submit" className={styles.btnInfo} block>{t('accountProfile.button.sendVerificationCode')}</Button>
+                    </Form.Item>
+                  </Col>
+                  <Col md={12} xs={64} offset="6">
+                    <Form.Item label={t('accountProfile.phone.otp')} name="otp">
+                      <Input maxLength={10} />
+                    </Form.Item>
+                  </Col>
+                  <Col md={12} xs={24} offset="6">
+                    <Form.Item>
+                      <Button  className={styles.btnPrimary} block>{t('accountProfile.button.addPhone')}</Button>
+                    </Form.Item>
+                  </Col>
+                </Row>
+              </Form>
             </Col> 
-        </Row>
+          </Row>
         </div>
-        </div>
+      </div>
     </main> 
   )
 }
