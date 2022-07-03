294c9321672a1ca93f369fbd76611c25b57900fe
Parinpond <parinpond.lag@gmail.com>
2022-07-03 11:37:10 +0700

    fix : indent

--- modules/Profile/delPhoneModal.tsx
+++ modules/Profile/delPhoneModal.tsx
@@ -1,25 +1,32 @@
 
-import React, {  FC,useEffect } from 'react'
-import { Typography, Button, Row, Col, Input, Modal } from 'antd'
+import React, {  FC } from 'react'
+import { Typography, Button, Col, Modal } from 'antd'
 import t from '~/locales'
 
 const { Text } = Typography
 interface IDelPhoneModalProps {
-    isOpen: boolean
-    toggle: () => void
+  isOpen: boolean
+  toggle: () => void
 }
 const DelPhoneModal: FC<IDelPhoneModalProps> = (props: IDelPhoneModalProps) => {
-      function toggle(): void {
-        props.toggle()
-      }
-    return (
-        <>
-      <Modal title={t('accountProfile.phone.deletePhone')} visible={props.isOpen} onCancel={toggle}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Modal>
-    </>
-      )
+  function toggle(): void {
+    props.toggle()
+  }
+  return (
+    <Modal title={t('accountProfile.phone.deletePhone')} visible={props.isOpen} onCancel={toggle}   
+      footer={[
+        <Col className='text-right'>
+          <Button type="default" onClick={toggle}>
+            {t('common.close')}
+          </Button>
+          <Button type="primary">
+            {t('common.confirm')}
+          </Button>
+        </Col>
+      ]}>
+      <Col> <i className='fas fas fa-exclamation-circle'/> <Text>{t('accountProfile.phone.confirmDelete')} 081-2226666</Text></Col>
+      <Col><Text type="danger">{t('accountProfile.phone.msgConfirmDelete')}</Text></Col>
+    </Modal>
+  )
 }
 export default DelPhoneModal
