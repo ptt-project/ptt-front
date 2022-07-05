5d5e841a3529e33979739890bbb149e2edccd4b9
Parinpond <parinpond.lag@gmail.com>
2022-07-05 22:38:29 +0700

    fix : refactor code

--- modules/Profile/Phone/index.tsx
+++ modules/Profile/Phone/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState } from 'react'
+import { NextRouter, useRouter } from 'next/router'
 import Link from 'next/link'
 import Helmet from 'react-helmet'
 import { Typography, Button, Row, Col } from 'antd'
@@ -7,12 +8,14 @@ import OtpModal from '~/components/main/OtpModal'
 import ConfirmationModal from '~/components/main/ConfirmationModal'
 import t from '~/locales'
 import { IOtpData } from '~/model/Common'
+import { CustomUrl } from '~/utils/main'
 import Breadcrumbs from '~/components/main/Breadcrumbs'
 import styles from './ProfilePhone.module.scss'
 
 const { Text } = Typography
 
 const Phone: FC = () => {
+  const router: NextRouter = useRouter()
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [isOpenDelPhoneModal, setIsOpenDelPhoneModal] = useState<boolean>(false)
   function toggle(): void {
@@ -35,14 +38,14 @@ const Phone: FC = () => {
   }
   return (
     <>
-      <OtpModal  mobileNo="12346" isOpen={isOpen} toggle={toggle} onSubmit={onSubmit} />
-      <ConfirmationModal 
-        isOpen={isOpenDelPhoneModal} 
-        toggle={toggleDelPhoneModal} 
-        type='error'
-        title= {t('accountProfile.phone.deletePhone')}
+      <OtpModal mobileNo="12346" isOpen={isOpen} toggle={toggle} onSubmit={onSubmit} />
+      <ConfirmationModal
+        isOpen={isOpenDelPhoneModal}
+        toggle={toggleDelPhoneModal}
+        type="error"
+        title={t('accountProfile.phone.deletePhone')}
         content={`${t('accountProfile.phone.confirmDelete')}081-2226666`}
-        contentWarning ={t('accountProfile.phone.msgConfirmDelete')}
+        contentWarning={t('accountProfile.phone.msgConfirmDelete')}
       />
       <main className="main account">
         <Helmet>
@@ -62,45 +65,41 @@ const Phone: FC = () => {
           <div className="container">
             <Row gutter={48}>
               <Col xl={6} lg={0}>
-                <SettingSidebar sidebarType="buyer"/>
+                <SettingSidebar sidebarType="buyer" />
               </Col>
               <Col xl={{ span: 15, offset: 1 }} lg={{ span: 18, offset: 3 }} md={24}>
                 <Text>
-                  <h4 className={`text-center mb-5 ${styles.textPrimary}`}>
+                  <h4 className={`text-center mb-5 ${styles.textSecondary}`}>
                     {t('accountProfile.phone.titleEdit')}
                   </h4>
                 </Text>
                 <Row gutter={[16, 8]}>
                   <Col md={12} xs={12}>
-                    <Text className={styles.textPrimary}>
+                    <Text className={styles.textSecondary}>
                       {t('accountProfile.phone.phoneList')}
                     </Text>
                   </Col>
                   <Col md={12} xs={12} className="text-right">
-                    <Link href="/settings/account/info/add-phone">
-                      <Button className={styles.textPrimary}>
-                        <i className={`fas fa-plus ${styles.paddingRight5}`} />
+                    <Link href={CustomUrl.href('/settings/account/info/add-phone', router.locale)}>
+                      <Button className={styles.textSecondary}>
+                        <i className="fas fa-plus mr-2" />
                         {t('accountProfile.button.addPhone')}
                       </Button>
                     </Link>
                   </Col>
                   <Col span={24} className="alert alert-message alert-light alert-primary">
-                    <Text className={`${styles.textUnderline} ${styles.paddingRight5}`}>
-                      081-111-1111
-                    </Text>
+                    <Text className={`mr-2 ${styles.textPrimary}`}>081-111-1111</Text>
                     <Button>
-                      <i className={`fas fa-star ${styles.paddingRight5}`} />
+                      <i className="fas fa-star mr-2" />
                       {t('accountProfile.button.mainNumber')}
                     </Button>
                   </Col>
                   <Col span={12}>
-                    <Text className={`${styles.textUnderline} ${styles.paddingRight5}`}>
-                      081-111-2222
-                    </Text>
+                    <Text className={`${styles.textPrimary}`}>081-111-2222</Text>
                   </Col>
                   <Col span={12} className="text-right">
-                    <i className={`fas fa-star ${styles.paddingRight5}`} onClick={onFavoritePhone} />
-                    <i className={`fas fa-trash-alt ${styles.paddingRight5}`} onClick={onDelPhoneModal} />
+                    <i className="fas fa-star mr-2" onClick={onFavoritePhone} />
+                    <i className="fas fa-trash-alt mr-2" onClick={onDelPhoneModal} />
                   </Col>
                 </Row>
               </Col>
