26417f07896ab7c3b0ece1712f67bd3b4b469e38
Parinpond <parinpond.lag@gmail.com>
2022-07-11 23:04:51 +0700

    fix : loop data

--- modules/Point/components/PointDetail/index.tsx
+++ modules/Point/components/PointDetail/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC,useState } from 'react'
 import { Button, Row, Col,Typography,Avatar,Image,Rate  } from 'antd'
 import t from '~/locales'
 import styles from './PointDetail.module.scss'
@@ -8,14 +8,14 @@ const data = [
   {
     title: 'Hannah',
     orderID: '2006306N01XXFD',
-    productName:'ดัมเบลหุ้มยาง 2 กิโลกรัม',
+    productName:'ดัมเบลหุ้มยาง 4 กิโลกรัม',
     status:'wait',
     avatar: 'https://joeschmoe.io/api/v1/random'
   },
   {
     title: 'Sophia',
     orderID: '2006306N01XXF1',
-    productName:'ดัมเบลหุ้มยาง 2 กิโลกรัม',
+    productName:'ดัมเบลหุ้มยาง 3 กิโลกรัม',
     status:'reply',
     avatar: 'https://joeschmoe.io/api/v1/random'
   },
@@ -29,12 +29,11 @@ const data = [
   {
     title: 'Caroline',
     orderID: '2006306N01XXF3',
-    productName:'ดัมเบลหุ้มยาง 2 กิโลกรัม',
+    productName:'ดัมเบลหุ้มยาง 1 กิโลกรัม',
     status:'wait',
     avatar: 'https://joeschmoe.io/api/v1/random'
   }
 ]
-
 const PointDetail: FC = () => (
   <>
     <Row >
@@ -50,44 +49,32 @@ const PointDetail: FC = () => (
       <Col lg={12}><Text type="danger">{t('shopPoint.reviewDetail')}</Text></Col>
       <Col lg={6}><Text type="danger">{t('shopPoint.yourReply')}</Text></Col>
     </Row>
-    <div className="mb-3">
-    <Row className={`mt-4 ${styles.tableTitle}`}>
-      <Col span={18}>{t('shopPoint.user')} : <Avatar src="https://joeschmoe.io/api/v1/random" /> Sara</Col>
-      <Col className="text-left mt-1" span={6}>{t('shopPoint.orderID')}: 2006306N01XXFD</Col>
-    </Row>
-    <Row >
-      <Col className={styles.productCol} span={6}>
-        <Image  width={50} src="https://joeschmoe.io/api/v1/random"/>
-        <Text>ดัมเบลหุ้มยาง 2 กิโลกรัม</Text>
-      </Col>
-      <Col className={styles.reviewCol} span={12}>
-        <Rate allowHalf defaultValue={3} />
-        <p>คุณภาพของสินค้าดีมาก ความคุ้มค่าคุ้มราคาดีมาก ความรวดเร็วในการจัดส่งดีมาก แนะนำยี่ห้อนี้ค่ะ</p>
-        <Text type='secondary'>23/06/2022 23:23</Text>
-      </Col>
-      <Col className={styles.replyCol} span={6}><Button className="mt-1">{t('shopPoint.reply')}</Button> </Col>
-    </Row>
-    </div>
-    <div className="mb-3">
-    <Row className={`mt-4 ${styles.tableTitle}`}>
-      <Col span={18}>{t('shopPoint.user')} : <Avatar src="https://joeschmoe.io/api/v1/random" /> Sara</Col>
-      <Col className="text-left mt-1" span={6}>{t('shopPoint.orderID')}: 2006306N01XXFD</Col>
-    </Row>
-    <Row >
-      <Col className={styles.productCol} span={6}>
-        <Image  width={50} src="https://joeschmoe.io/api/v1/random"/>
-        <Text>ดัมเบลหุ้มยาง 2 กิโลกรัม</Text>
-      </Col>
-      <Col className={styles.reviewCol} span={12}>
-        <Rate allowHalf defaultValue={3} />
-        <p>คุณภาพของสินค้าดีมาก ความคุ้มค่าคุ้มราคาดีมาก ความรวดเร็วในการจัดส่งดีมาก แนะนำยี่ห้อนี้ค่ะ</p>
-        <Text type='secondary'>23/06/2022 23:23</Text>
-      </Col>
-      <Col className={styles.replyCol} span={6}><Button className="mt-1">{t('shopPoint.reply')}</Button> </Col>
-    </Row>
-    </div>
-    
-
+    {data?.map((item, index) => {
+      return (
+        <div className="mb-3">
+          <Row className={`mt-4 ${styles.tableTitle}`}>
+            <Col span={18}>{t('shopPoint.user')} : <Avatar src="https://joeschmoe.io/api/v1/random" /> {item.title}</Col>
+            <Col className="text-left mt-1" span={6}>{t('shopPoint.orderID')}: <Text>{item.orderID}</Text></Col>
+          </Row>
+          <Row >
+            <Col className={styles.productCol} span={6}>
+              <Image  width={50} src="https://joeschmoe.io/api/v1/random"/>
+              <Text>{item.productName}</Text>
+            </Col>
+            <Col className={styles.reviewCol} span={12}>
+              <Rate allowHalf defaultValue={3} />
+              <p>คุณภาพของสินค้าดีมาก ความคุ้มค่าคุ้มราคาดีมาก ความรวดเร็วในการจัดส่งดีมาก แนะนำยี่ห้อนี้ค่ะ</p>
+              <Text type='secondary'>23/06/2022 23:23</Text>
+            </Col>
+            <Col className={styles.replyCol} span={6}>
+              {item.status === 'reply'?
+              <Button className="mt-1">{t('shopPoint.reply')}</Button> 
+            :<Text>{item.productName}</Text>}
+            </Col>
+          </Row>
+        </div>
+      )
+    })}
   </>
 )
 
