85167128823a23146a56829be65141014cb14df6
einost <einost_fi@hotmail.com>
2022-07-12 23:30:03 +0700

    feat!: [HAP-2] update layout header & footer

--- components/features/custom-link.jsx
+++ components/features/custom-link.jsx
@@ -1,30 +1,35 @@
-import Link from "next/link";
+import Link from 'next/link'
 
-import { parseContent } from '~/utils';
+import { parseContent } from '~/utils'
 
-export default function ALink ( { children, className, content, style, ...props } ) {
-
-    const preventDefault = ( e ) => {
-        if ( props.href === '#' ) {
-            e.preventDefault();
-        }
-
-        if ( props.onClick ) {
-            props.onClick();
-        }
+export default function ALink(props) {
+  const { children, className, content, style } = props
+  const preventDefault = (e) => {
+    if (props.href === '#') {
+      e.preventDefault()
     }
 
-    return (
-        content ?
-            <Link { ...props } >
-                <a className={ className } style={ style } onClick={ preventDefault } dangerouslySetInnerHTML={ parseContent( content ) }>
-                    { children }
-                </a>
-            </Link> :
-            <Link { ...props } >
-                <a className={ className } style={ style } onClick={ preventDefault }>
-                    { children }
-                </a>
-            </Link>
-    )
+    if (props.onClick) {
+      props.onClick()
+    }
+  }
+
+  return content ? (
+    <Link {...props}>
+      <a
+        className={className}
+        style={style}
+        onClick={preventDefault}
+        dangerouslySetInnerHTML={parseContent(content)}
+      >
+        {children}
+      </a>
+    </Link>
+  ) : (
+    <Link {...props}>
+      <a className={className} style={style} onClick={preventDefault}>
+        {children}
+      </a>
+    </Link>
+  )
 }
