fb28e96335ca8a4e0644acc60492ae87349c1713
einost <einost_fi@hotmail.com>
2022-07-19 20:50:51 +0700

    fix: eslint rule

--- modules/SellerPoint/index.tsx
+++ modules/SellerPoint/index.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react'
-import { NextRouter, useRouter } from 'next/router'
 import Helmet from 'react-helmet'
 import { Typography, Row, Col } from 'antd'
 import SettingSidebar from '~/components/main/SettingSidebar'
@@ -11,58 +10,54 @@ import styles from './SellerPoint.module.scss'
 
 const { Text, Title } = Typography
 
-const SellerPoint: FC = () => {
-  const router: NextRouter = useRouter()
-
-  return (
-    <main className="main">
-      <Helmet>
-        <title>
-          {t('meta.title')} | {t('sellerPoint.title')}
-        </title>
-      </Helmet>
-      <Breadcrumbs
-        items={[
-          { title: t('sellerPoint.shop') },
-          { title: t('sellerPoint.title'), href: '/seller/settings/shop/point' }
-        ]}
-      />
-      <div className="page-content mb-9">
-        <div className="container">
-          <Row gutter={48}>
-            <Col xl={6}>
-              <SettingSidebar sidebarType="seller" />
-            </Col>
-            <Col xl={18} lg={24}>
-              <Row className="mb-3" align="middle">
-                <Col xs={20}>
-                  <Title className={`${styles.h4} ${styles.textSecondary}`} level={4}>
-                    {t('sellerPoint.title')}
+const SellerPoint: FC = () => (
+  <main className="main">
+    <Helmet>
+      <title>
+        {t('meta.title')} | {t('sellerPoint.title')}
+      </title>
+    </Helmet>
+    <Breadcrumbs
+      items={[
+        { title: t('sellerPoint.shop') },
+        { title: t('sellerPoint.title'), href: '/seller/settings/shop/point' }
+      ]}
+    />
+    <div className="page-content mb-9">
+      <div className="container">
+        <Row gutter={48}>
+          <Col xl={6}>
+            <SettingSidebar sidebarType="seller" />
+          </Col>
+          <Col xl={18} lg={24}>
+            <Row className="mb-3" align="middle">
+              <Col xs={20}>
+                <Title className={`${styles.h4} ${styles.textSecondary}`} level={4}>
+                  {t('sellerPoint.title')}
+                </Title>
+                <Text type="secondary">{t('sellerPoint.detail')}</Text>
+              </Col>
+              <Col xs={4}>
+                <div className={styles.point}>
+                  <Title className={styles.h1} type="danger" level={1}>
+                    5.0
                   </Title>
-                  <Text type="secondary">{t('sellerPoint.detail')}</Text>
-                </Col>
-                <Col xs={4}>
-                  <div className={styles.point}>
-                    <Title className={styles.h1} type="danger" level={1}>
-                      5.0
-                    </Title>
-                    <Title className={`${styles.h4} ${styles.space}`} type="secondary" level={4}>
-                      /
-                    </Title>
-                    <Title className={styles.h4} type="secondary" level={4}>
-                      {t('sellerPoint.part')}
-                    </Title>
-                  </div>
-                </Col>
-              </Row>
-              <SellerPointFilters />
-              <SellerPointTabs />
-            </Col>
-          </Row>
-        </div>
+                  <Title className={`${styles.h4} ${styles.space}`} type="secondary" level={4}>
+                    /
+                  </Title>
+                  <Title className={styles.h4} type="secondary" level={4}>
+                    {t('sellerPoint.part')}
+                  </Title>
+                </div>
+              </Col>
+            </Row>
+            <SellerPointFilters />
+            <SellerPointTabs />
+          </Col>
+        </Row>
       </div>
-    </main>
-  )
-}
+    </div>
+  </main>
+)
 
 export default SellerPoint
