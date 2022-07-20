1b433a919aedb77ec40de089686ff9b58b82b1aa
Parinpond <parinpond.lag@gmail.com>
2022-07-20 17:56:30 +0700

    fix : edit form product

--- modules/SellerMyProducts/components/SellerMyProductsForm/components/Sales.tsx
+++ modules/SellerMyProducts/components/SellerMyProductsForm/components/Sales.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Typography, Radio, Col, Form, Input, Row, Select } from 'antd'
+import { Typography, Switch, Radio, Col, Form, Input, Row, Select } from 'antd'
 import HighlightLabel from '~/components/main/HighlightLabel'
 import t from '~/locales'
 import styles from '../SellerMyProductsForm.module.scss'
@@ -14,50 +14,57 @@ interface IFormProductSalesProps {
   disabled?: boolean
 }
 const Sales: React.FC<IFormProductSalesProps> = (props: IFormProductSalesProps) => {
+  const [isCheckUseOptions, setIsCheckUseOptions] = useState<boolean>(false)
+  const onChange = (checked: boolean) => {
+    setIsCheckUseOptions(checked)
+  }
   return (
     <>
       <HighlightLabel title={t('sellerProducts.form.sales.title')} />
       <Row gutter={[16, 8]}>
-        <Col md={12}></Col>
-        <Col md={12}>
-          <Form.Item
-            label={t('sellerProducts.form.features.brand')}
-            name="brand"
-            rules={[{ required: true }]}
-          >
-            <Select defaultValue="">
-              <Option value=""></Option>
-            </Select>
-          </Form.Item>
+        <Col md={24}>
+          <Switch onChange={onChange} />
+          <Text className="ml-2 mt-1">{t('sellerProducts.form.sales.useOptions')}</Text>
         </Col>
-        <Col md={12}>
-          <Form.Item
-            label={t('sellerProducts.form.features.weight')}
-            name="weight"
-            rules={[
-              {
-                required: true
-              }
-            ]}
-          >
-            <Input suffix={<Text type="secondary">{t('sellerProducts.form.features.kg')}</Text>} />
-          </Form.Item>
-        </Col>
-        <Col md={12}>
-          <Form.Item label={t('sellerProducts.form.features.shelfLife')} name="shelfLife">
-            <Input suffix={<Text type="secondary">{t('sellerProducts.form.features.day')}</Text>} />
-          </Form.Item>
-        </Col>
-        <Col md={12}>
-          <Form.Item label={t('sellerProducts.form.features.condition')} name="condition">
-            <Radio.Group>
-              <Radio value={1} className="mr-5">
-                {t('sellerProducts.form.features.old')}
-              </Radio>
-              <Radio value={2}>{t('sellerProducts.form.features.new')}</Radio>
-            </Radio.Group>
-          </Form.Item>
-        </Col>
+        {!isCheckUseOptions && (
+          <>
+            <Col md={12}>
+              <Form.Item
+                label={t('sellerProducts.form.sales.price')}
+                name="price"
+                rules={[
+                  {
+                    required: true
+                  }
+                ]}
+              >
+                <Input
+                  suffix={<Text type="secondary">{t('sellerProducts.form.sales.baht')}</Text>}
+                />
+              </Form.Item>
+            </Col>
+            <Col md={12}>
+              <Form.Item label={t('sellerProducts.form.sales.warehouse')} name="warehouse">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col md={12}>
+              <Form.Item
+                label={
+                  <Text>
+                    {t('sellerProducts.form.sales.sku')}
+                    <Text className="ml-1" type="secondary">
+                      {t('sellerProducts.form.sales.msgSku')}
+                    </Text>
+                  </Text>
+                }
+                name="sku"
+              >
+                <TextArea rows={1} showCount maxLength={20} />
+              </Form.Item>
+            </Col>
+          </>
+        )}
       </Row>
     </>
   )
