f97c1bdb08d84b69a0d3c2d6bfa23087109e6b37
miniBear <ax_zaa@windowslive.com>
2022-11-02 00:39:47 +0700

    fix: interface error

--- interfaces/cart.interface.ts
+++ interfaces/cart.interface.ts
@@ -1,14 +1,57 @@
-import { IProduct, IShop } from '.'
+import { IProduct, IProductProfile, IShop } from '.'
 
 export interface ICartProduct extends IProduct {
   index: number
-  name?: string
   amount?: number
   isProduct?: boolean
   isShop?: boolean
+
+  // ใส่ไปก่อน ให้ build ผ่าน อย่าลืมถาม logic แล้วแก้อีกที
+  stock: number
+  price: number
+  option1?: string
+  option2?: string
+  shop?: IShop
+  productProfile?: IProductProfile
+  createdAt: Date
+  updatedAt: Date
 }
 
 export interface ICartProductItem extends IShop {
   products: ICartProduct[]
   isProduct?: boolean
+}
+
+/**
+ * @deprecated รอพี่ยิ้มแก้ไปใช้ interface ตัวใหม่
+ */
+export interface IProductData {
+  /**
+   * @deprecated รอพี่ยิ้มแก้ไปใช้ interface ตัวใหม่
+   */
+  key: string
+  /**
+   * @deprecated รอพี่ยิ้มแก้ไปใช้ interface ตัวใหม่
+   */
+  productName: string
+  /**
+   * @deprecated รอพี่ยิ้มแก้ไปใช้ interface ตัวใหม่
+   */
+  brand: string
+  /**
+   * @deprecated รอพี่ยิ้มแก้ไปใช้ interface ตัวใหม่
+   */
+  amount: number
+  /**
+   * @deprecated รอพี่ยิ้มแก้ไปใช้ interface ตัวใหม่
+   */
+  quantity: number
+  /**
+   * @deprecated รอพี่ยิ้มแก้ไปใช้ interface ตัวใหม่
+   */
+  sold: number
+  /**
+   * @deprecated รอพี่ยิ้มแก้ไปใช้ interface ตัวใหม่
+   */
+  status: number
 }
